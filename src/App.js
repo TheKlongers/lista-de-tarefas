39b23c8232644798d69f9380439720644b210f02
unknown <klingerribeiro@outlook.com>
2023-07-08 12:29:44 -0300

    atualização 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
+  const handleChange = (event) => {
+    setTask(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (task) {
+      setTaskList([...taskList, task]);
+      setTask('');
+    }
+  };
+
+  const handleDelete = (index) => {
+    const updatedTaskList = [...taskList];
+    updatedTaskList.splice(index, 1);
+    setTaskList(updatedTaskList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Lista de Tarefas</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={task}
+          onChange={handleChange}
+          placeholder="Digite uma tarefa"
+        />
+        <button type="submit">Adicionar</button>
+      </form>
+      <ul>
+        {taskList.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => handleDelete(index)}>Remover</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
